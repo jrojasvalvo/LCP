28e28e9819a5938870e076b0683aeeedce44416b
levekyle <kleve@andrew.cmu.edu>
2021-02-13 16:38:03 -0500

    .

--- Stressed-Cat/Assets/Sound_Manager.cs
+++ Stressed-Cat/Assets/Sound_Manager.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Sound_Manager : MonoBehaviour
 {
+<<<<<<< Updated upstream
     private AudioSource backgroundMusic;
+=======
+    private AudioSource backgroundmusic;
+>>>>>>> Stashed changes
 
     // Start is called before the first frame update
     void Start()
     {
+<<<<<<< Updated upstream
         backgroundMusic = GetComponent<AudioSource>();
 
         backgroundMusic.Stop();
         backgroundMusic.Play();
+=======
+        backgroundmusic = GetComponent<AudioSource>();
+
+        backgroundmusic.Stop();
+        backgroundmusic.Play();
+>>>>>>> Stashed changes
     }
 
     // Update is called once per frame
