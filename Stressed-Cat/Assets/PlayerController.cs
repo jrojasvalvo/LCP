580d4f3765cb3cfb38e33f6ba1ee7f7366ec4e00
Shmeigo <jlessiohadi@gmail.com>
2021-02-13 17:52:47 -0500

    Player doesn't just fly through platforms

--- Stressed-Cat/Assets/PlayerController.cs
+++ Stressed-Cat/Assets/PlayerController.cs
@@ -24,6 +24,7 @@ public class PlayerController : MonoBehaviour
     public float initial_y = -3.5f;
 
     public bool dead;
+    public bool canJump = true;
     
     void Start()
     {
@@ -47,7 +48,7 @@ public class PlayerController : MonoBehaviour
             //Jumping and Climbing
             if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.W))
             {
-                if (grounded)
+                if (grounded && canJump)
                 {
                     rb.velocity = new Vector2(rb.velocity.x, jump);
                     fastFall = 0f;
@@ -57,6 +58,15 @@ public class PlayerController : MonoBehaviour
                 if (!grounded){
                     fastFall = fastFallSpeed;
                 }
+            }
+            //can't just hold down the jump button and keep jumping
+            if (Input.GetKeyUp(KeyCode.Space) || Input.GetKeyUp(KeyCode.UpArrow) 
+                || Input.GetKeyUp(KeyCode.Z) || Input.GetKeyUp(KeyCode.W)) {
+                    canJump = true;
+            }
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) 
+                || Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.W)) {
+                    canJump = false;
             }
 
             if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
