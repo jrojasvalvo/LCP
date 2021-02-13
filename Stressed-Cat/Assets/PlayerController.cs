b44dacfa222be8587da4fbd2214ec24159e6c267
annehe9 <afhe@andrew.cmu.edu>
2021-02-12 22:55:37 -0500

    First commit to movement branch

---
+++ Stressed-Cat/Assets/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    //Movement
+    public float speed;
+    public float jump;
+    float moveVelocity;
+
+    //Grounded Vars
+    bool grounded = true;
+    
+    void Start()
+    {
+        rb = this.gameObject.GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        //Jumping
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.W))
+        {
+            if (grounded)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jump);
+            }
+        }
+
+        moveVelocity = 0;
+
+        //Left Right Movement
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            moveVelocity = -speed;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            moveVelocity = speed;
+        }
+
+        rb.velocity = new Vector2(moveVelocity, rb.velocity.y);
+
+    }
+    //Check if Grounded
+    void OnTriggerEnter2D()
+    {
+        grounded = true;
+    }
+    void OnTriggerExit2D()
+    {
+        grounded = false;
+    }
+}
