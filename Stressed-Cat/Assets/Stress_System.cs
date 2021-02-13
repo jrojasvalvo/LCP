331fde86944c5a8a5cb9e40f5cbb433aaa095ff1
jrojasvalvo <jrojasva@andrew.cmu.edu>
2021-02-12 23:03:28 -0500

    Donut Collection

--- Stressed-Cat/Assets/Stress_System.cs
+++ Stressed-Cat/Assets/Stress_System.cs
@@ -8,12 +8,15 @@ public class Stress_System : MonoBehaviour
     public float stress_level = 0f;
     private bool startled = false;
     public GameObject enemies;
+    public GameObject donuts;
     private Transform[] all_enemies;
+    private Transform[] all_donuts;
 
     void Start()
     {
         startled = false;
         all_enemies = enemies.GetComponentsInChildren<Transform>();
+        all_donuts = donuts.GetComponentsInChildren<Transform>();
     }
 
     // Update is called once per frame
@@ -21,15 +24,24 @@ public class Stress_System : MonoBehaviour
     {   
         float player_x = transform.position.x;
         float player_y = transform.position.y;
+
+        //Stress Proximity Variables:
         float enemy_x;
         float enemy_y;
+        
+        //Donut Proximity Variables:
+        float donut_x;
+        float donut_y;
+
         float x_diff;
         float y_diff;
         float dist;
 
+
         if(stress_level >= max_stress) {
             startled = true;
         }
+        //Check for proximity to enemies to increase stress level:
         for(int i = 0; i < all_enemies.Length; i++) {
             enemy_x = all_enemies[i].position.x;
             enemy_y = all_enemies[i].position.y;
@@ -38,6 +50,18 @@ public class Stress_System : MonoBehaviour
             dist = (float)(Math.Sqrt(x_diff * x_diff + y_diff * y_diff));
             if(dist <= 5) {
                 stress_level += (5 - dist) / 100;
+            }
+        }
+        //Check if the player is close enough to any donuts to collect them:
+        for(int i = 0; i < all_donuts.Length; i++) {
+            donut_x = all_donuts[i].position.x;
+            donut_y = all_donuts[i].position.y;
+            x_diff = Math.Abs(player_x - donut_x);
+            y_diff = Math.Abs(player_y - donut_y);
+            dist = (float)(Math.Sqrt(x_diff * x_diff + y_diff * y_diff));
+            if(dist <= 0.6 && all_donuts[i].gameObject.active) {
+                all_donuts[i].gameObject.SetActive(false);
+                stress_level -= 20;
             }
         }
         print(stress_level);
