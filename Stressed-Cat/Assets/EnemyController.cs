906b2b4eb005ba045e3e613ce471a3857ad6cc7f
annehe9 <afhe@andrew.cmu.edu>
2021-02-13 00:43:10 -0500

    Enemy movement script, single jump fix, climbing

---
+++ Stressed-Cat/Assets/EnemyController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed;
+    private float direction;
+    private bool facingRight;
+    public float rightBoundary;
+    public float leftBoundary;
+
+    void Start()
+    {
+        facingRight = true;
+        direction = 1;
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 movement = new Vector2(speed * direction, 0);
+        transform.Translate(movement * Time.deltaTime);
+        if (facingRight && transform.position.x > rightBoundary)
+        {
+            facingRight = false;
+            direction = -1;
+
+        }
+        if (!facingRight && transform.position.x < leftBoundary)
+        {
+            facingRight = true;
+            direction = 1;
+
+        }
+    }
+
+    void LineOfSight()
+    {
+
+    }
+}
--- Stressed-Cat/Assets/PlayerController.cs
+++ Stressed-Cat/Assets/PlayerController.cs
@@ -8,25 +8,36 @@ public class PlayerController : MonoBehaviour
     //Movement
     public float speed;
     public float jump;
+    private bool climb;
     float moveVelocity;
 
     //Grounded Vars
-    bool grounded = true;
+    bool grounded;
     
     void Start()
     {
         rb = this.gameObject.GetComponent<Rigidbody2D>();
+        grounded = true;
+        climb = false;
     }
 
 
     void Update()
     {
-        //Jumping
+        //Jumping and Climbing
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.W))
         {
             if (grounded)
             {
                 rb.velocity = new Vector2(rb.velocity.x, jump);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (climb)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, -jump);
             }
         }
 
@@ -46,12 +57,31 @@ public class PlayerController : MonoBehaviour
 
     }
     //Check if Grounded
-    void OnTriggerEnter2D()
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Ground") grounded = true;
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Ground") grounded = false;
+        //replace with object(s) name
+        if (col.gameObject.tag == "Ladder")
+        {
+            climb = false;
+            rb.gravityScale = 1.0f;
+            rb.velocity = new Vector2(rb.velocity.x,0);
+        }
+    }
+
+    //Check if on Climbable Object
+    void OnTriggerStay2D(Collider2D col)
     {
         grounded = true;
-    }
-    void OnTriggerExit2D()
-    {
-        grounded = false;
+        //replace with object(s) name
+        if (col.gameObject.tag == "Ladder")
+        {
+            climb = true;
+            rb.gravityScale = 0.0f;
+        }
     }
 }
