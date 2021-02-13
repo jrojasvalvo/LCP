3a23784e09ea994ef963e2ce493409cfe6282fc6
Shmeigo <jlessiohadi@gmail.com>
2021-02-13 11:38:14 -0500

    Can high/low jump

--- Stressed-Cat/Assets/PlayerController.cs
+++ Stressed-Cat/Assets/PlayerController.cs
@@ -28,14 +28,13 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //Jumping and Climbing
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.W))
+        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.W))
         {
-            if (grounded && canJump)
+            if (grounded)
             {
                 rb.velocity = new Vector2(rb.velocity.x, jump);
                 fastFall = 0f;
             }
-            canJump = false;
         } else {
             if (!grounded){
                 fastFall = fastFallSpeed;
@@ -63,6 +62,7 @@ public class PlayerController : MonoBehaviour
         }
 
         rb.velocity = new Vector2(moveVelocity, rb.velocity.y - fastFall);
+        Debug.Log(rb.velocity.y); 
 
     }
     //Check if Grounded
