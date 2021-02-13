90a51f175fa75054d8875fb4c6aa33faf81cd181
annehe9 <afhe@andrew.cmu.edu>
2021-02-13 01:11:06 -0500

    enemy line of sight

--- Stressed-Cat/Assets/EnemyController.cs
+++ Stressed-Cat/Assets/EnemyController.cs
@@ -10,14 +10,21 @@ public class EnemyController : MonoBehaviour
     public float rightBoundary;
     public float leftBoundary;
 
+    //Line of Sight variables
+    public float angle;
+    public float radius;
+    private GameObject Player;
+
     void Start()
     {
         facingRight = true;
         direction = 1;
+        Player = GameObject.Find("Cat");
     }
 
     void FixedUpdate()
     {
+        //Movement
         Vector2 movement = new Vector2(speed * direction, 0);
         transform.Translate(movement * Time.deltaTime);
         if (facingRight && transform.position.x > rightBoundary)
@@ -31,6 +38,15 @@ public class EnemyController : MonoBehaviour
             facingRight = true;
             direction = 1;
 
+        }
+
+        //Line of Sight
+        Vector2 player_pos = Player.transform.position;
+        Vector2 forward = new Vector2(direction, 0);
+        Vector2 v = player_pos - (Vector2)transform.position;
+        if (Mathf.Abs(Vector2.Angle(forward, v)) < angle && Vector2.Distance(player_pos, (Vector2)transform.position) < radius)
+        {
+            Debug.Log("uwu hello there");
         }
     }
 
