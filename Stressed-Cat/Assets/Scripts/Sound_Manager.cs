1b233203a74745c1f659211999367eb4a3b64c33
levekyle <kleve@andrew.cmu.edu>
2021-02-13 21:02:20 -0500

    plays

---
+++ Stressed-Cat/Assets/Scripts/Sound_Manager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound_Manager : MonoBehaviour
+{
+    private AudioSource backgroundMusic;
+    private AudioSource WinMusic;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource[] sound = GetComponents<AudioSource>();
+        backgroundMusic = sound[0];
+        WinMusic = sound[1];
+        backgroundMusic.Stop();
+        backgroundMusic.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayWinMusic()
+    {
+        backgroundMusic.Stop();
+        WinMusic.Stop();
+        WinMusic.Play();
+    }
+}
